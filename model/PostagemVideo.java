028e7fb4130797b4d828ec1c774e911b18dc4da1
NunesIcaro <icaronunes999@gmail.com>
2025-10-03 18:17:15 -0300

    ajustando as classes que extendem de postagem (postagemFoto e PostagemVideo) para integração com o InstagramService.
    -Atributos das classes filhas criados;
    -Constructor extendendo da classe pai nas classes filhas;
    -função exibir sobreescrevendo a função da classe pai;

--- model/PostagemVideo.java
+++ model/PostagemVideo.java
@@ -1,5 +1,18 @@
 package model;
 
-public class PostagemVideo {
-    
+public class PostagemVideo extends Postagem {
+    private String urlVideo;
+    private int duracao;
+
+    public PostagemVideo(Usuario autor,String urlVideo,int duracao){
+        super(autor);
+        this.urlVideo = urlVideo;
+        this.duracao = duracao;
+    }
+
+    @Override
+    public void exibir() {
+        System.out.println(urlVideo);
+        System.out.println(duracao);
+    }
 }
